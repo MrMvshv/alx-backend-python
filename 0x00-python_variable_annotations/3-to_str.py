fe8799f1bc4031098a3a50b514beed9601071004
MrMvshv <machariajosphat001@gmail.com>
2023-07-08 19:12:43 +0000

    python backend.4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str
+takes a float n as argument
+returs the string repr of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of n"""
+    return str(n)
