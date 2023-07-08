b07f08e51dceffffcb49748a9994650cb9eed285
MrMvshv <machariajosphat001@gmail.com>
2023-07-08 19:04:53 +0000

    python backend.2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function concat
+takes a string 1 and a string 2 as arguments
+returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the sum of a and b as a float"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor
+takes a float n as argument
+returs the dloor of the float(int)
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of n"""
+    return math.floor(n)
