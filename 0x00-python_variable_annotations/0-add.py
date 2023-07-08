d47e43e1882441ba8215b223b9dc3975f4e9b1ea
MrMvshv <machariajosphat001@gmail.com>
2023-07-08 18:57:46 +0000

    python backend.0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add
+takes a float a and a float b as arguments
+returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """returns the sum of a and b as a float"""
+    return a + b
