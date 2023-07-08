d4313e27ffe1ce4cb7e8028d9353fd7c7daef9f5
MrMvshv <machariajosphat001@gmail.com>
2023-07-08 20:12:15 +0000

    python backend.advanced done

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,12 +4,10 @@ Type-annotated function
 return dct[key]
 """
 from typing import TypeVar, Union, Mapping, Any
-
-
-
 T = TypeVar('T')
 Res = Union[Any, T]
 Def = Union[T, None]
+
 
 def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     """ returns the value or default(none)
