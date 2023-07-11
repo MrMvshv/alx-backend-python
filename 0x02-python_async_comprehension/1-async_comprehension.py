10e027678ec32751f37849f3df470f853729a032
MrMvshv <machariajosphat001@gmail.com>
2023-07-11 16:08:56 +0000

    import fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,20 +6,7 @@
 from typing import List
 import asyncio
 
-import importlib.util
-
-# Set the path to the module file
-module_path = './0-async_generator.py'
-
-# Load the module spec and create a module object
-spec = importlib.util.spec_from_file_location('module_name', module_path)
-module = importlib.util.module_from_spec(spec)
-
-# Execute the module to make its contents available
-spec.loader.exec_module(module)
-
-# Import the desired function from the module object
-async_generator = module.async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,20 +7,7 @@
 import time
 import asyncio
 
-import importlib.util
-
-# Set the path to the module file
-module_path = './1-async_comprehension.py'
-
-# Load the module spec and create a module object
-spec = importlib.util.spec_from_file_location('module_name', module_path)
-module = importlib.util.module_from_spec(spec)
-
-# Execute the module to make its contents available
-spec.loader.exec_module(module)
-
-# Import the desired function from the module object
-async_comprehension = module.async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
